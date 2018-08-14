ab6bc50fb8cfe89db48db6218d0c18413d75a75e
mashrur29 <mashrur639@gmail.com>
2018-08-14 23:14:03 +0600

    K-means

--- k_means_clustering.py
+++ k_means_clustering.py
@@ -25,4 +25,30 @@ variable_names = iris.feature_names
 
 clustering = KMeans(n_clusters=3, random_state=5)
 clustering.fit(X)
+# print(clustering.labels_)     # Print Labels
+
+# Plotting Model Output
+
+iris_df = pd.DataFrame(iris.data)
+iris_df.columns = ['Sepal_Length', 'Sepal_Width', 'Petal_Length', 'Petal_Width']
+y.columns = ['Target']
+
+
+relabel = np.choose(clustering.labels_, [2, 0, 1]).astype(np.int64)     # Relabel to match with output color, this is optional
+
+# relabel is the output of clustering
+
+color_theme = np.array(['darkgray', 'lightsalmon', 'powderblue'])
+
+plt.subplot(1, 2, 1)
+plt.scatter(x=iris_df.Petal_Length, y=iris_df.Petal_Width, c=color_theme[iris.target], s=50)
+plt.title('Ground Truth Classification')
+
+plt.subplot(1, 2, 2)
+plt.scatter(x=iris_df.Petal_Length, y=iris_df.Petal_Width, c=color_theme[relabel], s=50)
+plt.title('K-Means Clustering')
+
+plt.show()
+
+print(classification_report(y, relabel))
 
